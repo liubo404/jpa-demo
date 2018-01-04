1492fe7e4873fd5c71d22bdffa678f09e7998144
liubo <liubo404@gmail.com>
2018-01-04 17:40:00 +0800

    add basePackageClasses=TodoRepository.class to your @EnabledJpaRepositories annotation

--- tutorial/src/main/java/xxx/com/tutorial/config/JpaConfig.java
+++ tutorial/src/main/java/xxx/com/tutorial/config/JpaConfig.java
@@ -4,9 +4,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import xxx.com.tutorial.common.BaseRepositoryImpl;
+import xxx.com.tutorial.repository.TodoRepository;
 
 @Configuration
 @EnableJpaRepositories(
+        basePackageClasses = TodoRepository.class,
         repositoryBaseClass = BaseRepositoryImpl.class
 )
 @EnableTransactionManagement
